9b0a334ad9bf81abe4c0346c5d4fbd1a6d1c9812
ThanadechHub <57976945+ThanadechHub@users.noreply.github.com>
2026-04-06 16:49:26 +0700

    Update fibonacci.sh

--- scripts/fibonacci.sh
+++ scripts/fibonacci.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
-
-# Fibonacci sequence script
-
-read -p "Enter the number of terms: " n
+N=${1:-10}
 
 a=0
 b=1
 
-if [ $n -le 0 ] ; then
-    echo "Please enter a positive integer."
-else
-    echo "Fibonacci Sequence:"
-    for (( i=0; i<n; i++ )) ; do
-        echo $a
-        fn=$((a + b))
-        a=$b
-        b=$fn
-    done
-fi
+echo "The Fibonacci series is : "
+
+for (( i=0; i<N; i++ ))
+do
+    echo -e "$i\t$a"
+    fn=$((a + b))
+    a=$b
+    b=$fn
+done
